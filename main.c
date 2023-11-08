bb5b02c58e3e313a95681d6ce5311720f995b073
“Ejiro-David” <daviderhabor.de@gmail.com>
2023-11-08 10:35:15 +0100

    fixed main function absence, some specifiers failn

--- main.c
+++ main.c
@@ -19,15 +19,14 @@ int print_f(const char *format, ...)
             {
             case 'd':
                 count += fprintf(stdout, "%d", va_arg(args, int));
-            case 'c':
-                count += fprintf(stdout, "%c", va_arg(args, char));
+            // case 'c':
+            //     count += fprintf(stdout, "%c", va_arg(args, char));
             case 's':
                 count += fprintf(stdout, "%s", va_arg(args, char *));
             case 'f':
                 count += fprintf(stdout, "%f", va_arg(args, double));
             default:
                 putchar('%');
-                ;
                 putchar(*format);
                 count += 2;
                 break;
@@ -38,4 +37,28 @@ int print_f(const char *format, ...)
 
     va_end(args);
     return count;
+}
+
+
+int main(void)
+{
+    int age = 25;
+    print_f("My name is %s and I am %d years old.\n", "Snehasish", age);
+    double pi = 3.14;
+    print_f("The value of pi is %.2f.\n", pi);
+    // Print a date and time.
+    print_f("%s %s\n", "Today is", __DATE__);
+    print_f("%s %s\n", "The time is", __TIME__);
+
+    // Print a list of numbers.
+    int numbers[] = {1, 2, 3, 4, 5};
+    for (int i = 0; i < sizeof(numbers) / sizeof(numbers[0]); i++)
+    {
+        print_f("%d\n", numbers[i]);
+    }
+
+    // Print a table of data.
+    print_f("| Name | Age | Occupation |\n");
+    print_f("| Snehasish | 25 | AI Engineer |\n");
+    print_f("| Alice | 30 | Software Engineer |\n");
 }
