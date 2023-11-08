056fd20dd965a981be349a6353fcc074f9866b6e
“Ejiro-David” <daviderhabor.de@gmail.com>
2023-11-08 09:28:10 +0100

    Studied args

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # myprintf
+%d or %i: Used to print integers
+%f: Used to print floating point numbers
+%c: Used to print characters
+%s: Used to print strings
+%x or %X: Used to print hexadecimal numbers
+%p: Used to print pointers
+%%: Used to print the % character
--- concepts/arg.c
+++ concepts/arg.c
@@ -3,6 +3,26 @@
 #include <stdlib.h>
 
 int main (){
-    char *userIn
+    char *userInput  = NULL;
+
+    printf("$ ");
+
+    size_t bufferSize = 128;
+    userInput = (char *)malloc(bufferSize * sizeof(char));
+
+    if (userInput == NULL){
+        return 1;
+    }
+
+    if (fgets(userInput, bufferSize, stdin) != NULL){
+        printf("%s", userInput);
+    }else{
+        return -1;
+    }
+
+
+
+    return 0;
+
 }
 
