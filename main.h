caa09400a6d8ce51fcc69f1b784511015a1a7fd6
“Ejiro-David” <daviderhabor.de@gmail.com>
2023-11-08 10:26:54 +0100

    code does not compile yet

--- main.h
+++ main.h
@@ -1,4 +1,11 @@
-// prototypes go here
-#ifdef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+int print_f(const char *format, ...);
+
+
+#endif
